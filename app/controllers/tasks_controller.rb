e3bb62983ac4133d02a6d4463bda9da02f18dc1b
Bill Ingram <waingram@gmail.com>
2011-09-21 11:42:57 -0500

    Add create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,13 @@
 class TasksController < ApplicationController
-  respond_to :html, :json
+  respond_to :json
 
   def index
     respond_with(@tasks = Task.all)
   end
+
+  def create
+    task = Task.create(params)
+    respond_with task
+  end
+
 end
